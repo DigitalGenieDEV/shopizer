9ca3c6327ec3c1aa4372b7fa9fa61dc3c3670f81
yuhuei0398 <yuguanghui0398@163.com>
2024-07-02 23:21:10 +0800

    Product optimization

--- sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/product/PersistableProductMapper.java
+++ sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/product/PersistableProductMapper.java
@@ -100,7 +100,7 @@ public class PersistableProductMapper implements Mapper<PersistableProduct, Prod
 				destination.setSku(source.getSku() == null? source.getIdentifier() : source.getSku());
 			}
 
-			destination.setPublishWayEnums(PublishWayEnums.valueOf(source.getPublishWay()));
+			destination.setPublishWay(PublishWayEnums.valueOf(source.getPublishWay()));
 			destination.setLeftCategoryId(source.getLeftCategoryId() == null ? null :source.getLeftCategoryId());
 			destination.setAvailable(source.isVisible());
 			destination.setDateAvailable(new Date());
