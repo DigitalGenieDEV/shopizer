329e361d57b12f2178fc4d02705c0e339f2559c5
yuhuei0398 <yuguanghui0398@163.com>
2024-07-31 22:56:15 +0800

    fix product delete

--- sm-shop/src/main/java/com/salesmanager/shop/application/config/DocumentationConfiguration.java
+++ sm-shop/src/main/java/com/salesmanager/shop/application/config/DocumentationConfiguration.java
@@ -44,7 +44,7 @@ public class DocumentationConfiguration {
 
 //		private static final String HOST = "localhost:8080";
 
-	private static final String HOST = "54.180.87.211:8080";
+	private static final String HOST = "13.125.162.112:8080";
 //	private static final String HOST = "192.168.0.33";
 
 	/**
