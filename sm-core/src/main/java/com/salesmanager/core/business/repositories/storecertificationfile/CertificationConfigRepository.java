8ca63130f6a1ab5e632c378ee772896690247ba6
ByeongEon <lockon2200@ckbridge.co.kr>
2024-08-19 15:54:14 +0900

    update properties

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/storecertificationfile/CertificationConfigRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/storecertificationfile/CertificationConfigRepository.java
@@ -1,12 +1,10 @@
 package com.salesmanager.core.business.repositories.storecertificationfile;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.salesmanager.core.model.merchant.certificationfile.CertificationConfigEntity;
 
