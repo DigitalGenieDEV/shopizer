169b95e165b2c3698fc3833e874d7b1f082c12ab
yuhuei0398 <yuguanghui0398@163.com>
2024-09-03 21:51:12 +0800

    add

---
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/feature/ProductFeatureRepositoryCustom.java
@@ -0,0 +1,17 @@
+package com.salesmanager.core.business.repositories.catalog.product.feature;
+
+import com.salesmanager.core.business.services.catalog.product.ProductService;
+import com.salesmanager.core.model.catalog.product.*;
+import com.salesmanager.core.model.merchant.MerchantStore;
+import com.salesmanager.core.model.reference.language.Language;
+import com.salesmanager.core.model.tax.taxclass.TaxClass;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+public interface ProductFeatureRepositoryCustom {
+
+
+
+}
